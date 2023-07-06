3a493e62a04b8f27b2497a2c972dee4cfbdf6685
Baris <82235200+baak287@users.noreply.github.com>
2023-07-06 10:28:12 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-lftp -c 'set sftp:auto-confirm yes; set sftp:connect-program "ssh -a -x -oHostKeyAlgorithms=+ssh-rsa"; open -p '"$INPUT_PORT"' -u '"$INPUT_USER,$INPUT_PASSWORD"' sftp://'"$INPUT_SERVER"'; mirror -e -R -p -v '"$INPUT_LOCAL $INPUT_REMOTE"'; quit;'
+lftp -c 'set sftp:auto-confirm yes; set sftp:connect-program "ssh -a -x -oHostKeyAlgorithms=+ssh-rsa"; open -p '"$INPUT_PORT"' -u '"$INPUT_USER,$INPUT_PASSWORD"' sftp://'"$INPUT_SERVER"'; mirror -R -p -v '"$INPUT_LOCAL $INPUT_REMOTE"'; quit;'
